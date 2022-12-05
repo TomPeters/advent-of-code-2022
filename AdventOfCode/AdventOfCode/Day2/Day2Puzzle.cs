2f36653fbcbeb87eea1c83a1c606b40be02762ab
Tom Peters <tom.peters@octopus.com>
2022-12-05 20:15:18 +1000

    Day 2 part 1

--- AdventOfCode/AdventOfCode/Day2/Day2Puzzle.cs
+++ AdventOfCode/AdventOfCode/Day2/Day2Puzzle.cs
@@ -2,8 +2,81 @@ namespace AdventOfCode.Day2;
 
 public static class Day2Puzzle
 {
-    public static int GetTotalScore(string input)
+    public static int GetTotalScore(Game input)
     {
-        return 0;
+        return input.GetTotalScore();
     }
+}
+
+public record Game(Round[] Rounds)
+{
+    public int GetTotalScore() => Rounds.Sum(r => r.GetScore());
+}
+
+public record Round(HandShape OpponentsHandShape, HandShape PlayersHandShape)
+{
+    public int GetScore()
+    {
+        var result = GetResult();
+        return GetScoreForResult(result) + GetHandScore(PlayersHandShape);
+    }
+
+    static int GetHandScore(HandShape handShape)
+    {
+        return handShape switch
+        {
+            HandShape.Rock => 1,
+            HandShape.Paper => 2,
+            HandShape.Scissors => 3,
+            _ => throw new ArgumentOutOfRangeException(nameof(handShape), handShape, null)
+        };
+    }
+
+    static int GetScoreForResult(Result result) =>
+        result switch
+        {
+            Result.Lose => 0,
+            Result.Draw => 3,
+            Result.Win => 6,
+            _ => throw new ArgumentOutOfRangeException(nameof(result), result, null)
+        };
+    
+    Result GetResult()
+    {
+        return OpponentsHandShape switch
+        {
+            HandShape.Paper => PlayersHandShape switch
+            {
+                HandShape.Paper => Result.Draw,
+                HandShape.Rock => Result.Lose,
+                HandShape.Scissors => Result.Win
+            },
+            HandShape.Rock => PlayersHandShape switch
+            {
+                HandShape.Paper => Result.Win,
+                HandShape.Rock => Result.Draw,
+                HandShape.Scissors => Result.Lose
+            },
+            HandShape.Scissors => PlayersHandShape switch
+            {
+                HandShape.Paper => Result.Lose,
+                HandShape.Rock => Result.Win,
+                HandShape.Scissors => Result.Draw
+            }
+        };
+    }
+
+    enum Result
+    {
+        Lose,
+        Draw,
+        Win
+    }
+}
+
+public enum HandShape
+{
+    Rock,
+    Paper,
+    Scissors
 }
--- AdventOfCode/AdventOfCodeTests/Day2/Day2Tests.cs
+++ AdventOfCode/AdventOfCodeTests/Day2/Day2Tests.cs
@@ -1,4 +1,5 @@
-using AdventOfCode.Day1;
+using System;
+using System.Linq;
 using AdventOfCode.Day2;
 using Xunit;
 
@@ -9,7 +10,45 @@ public class Day2Tests
     [Fact]
     public void Part1_WorksForSampleData()
     {
-        var input = FileHelper.ReadFromFile("Day2", "Sample.txt");
-        Assert.Equal(15, Day2Puzzle.GetTotalScore(input));
+        var game = ParseInput(FileHelper.ReadFromFile("Day2", "Sample.txt"));
+        Assert.Equal(15, Day2Puzzle.GetTotalScore(game));
     }
+
+    [Fact]
+    public void Part2_WorksForRealData()
+    {
+        var game = ParseInput(FileHelper.ReadFromFile("Day2", "RealData.txt"));
+        Assert.Equal(11449, Day2Puzzle.GetTotalScore(game));
+    }
+
+    static Game ParseInput(string input)
+    {
+        var rounds = input.Split("\n").Select(roundInput =>
+        {
+            var shapesInput = roundInput.Split(" ");
+            var opponentHandShapeString = shapesInput[0];
+            var playersHandShapeString = shapesInput[1];
+            return new Round(GetOpponentsHandShape(opponentHandShapeString),
+                GetPlayersHandShape(playersHandShapeString));
+        }).ToArray();
+        return new Game(rounds);
+    }
+
+    static HandShape GetOpponentsHandShape(string input) =>
+        input switch
+        {
+            "A" => HandShape.Rock,
+            "B" => HandShape.Paper,
+            "C" => HandShape.Scissors,
+            _ => throw new ArgumentOutOfRangeException(input)
+        };
+    
+    static HandShape GetPlayersHandShape(string input) =>
+        input switch
+        {
+            "X" => HandShape.Rock,
+            "Y" => HandShape.Paper,
+            "Z" => HandShape.Scissors,
+            _ => throw new ArgumentOutOfRangeException(input)
+        };
 }
