e897550cc7767fe64a97110e179c41653c79eb5f
Tom Peters <tom.peters@octopus.com>
2022-12-05 19:17:06 +1000

    Day1 part 2

--- AdventOfCode/AdventOfCode/Day1/Day1Puzzle.cs
+++ AdventOfCode/AdventOfCode/Day1/Day1Puzzle.cs
@@ -2,11 +2,16 @@ namespace AdventOfCode.Day1;
 
 public static class Day1Puzzle
 {
-    public static int GetResult(Elf[] elves)
+    public static int GetHighestCalorieCountForASingleElf(Elf[] elves)
     {
         var elfCarryingTheMostCalories = elves.MaxBy(e => e.GetTotalCalories());
         if (elfCarryingTheMostCalories is null) throw new Exception("No elf found with the most calories");
         return elfCarryingTheMostCalories.GetTotalCalories();
+    }
+
+    public static int GetTotalCaloriesCarriedByTop3Elves(Elf[] elves)
+    {
+        return elves.OrderByDescending(e => e.GetTotalCalories()).Take(3).Sum(e => e.GetTotalCalories());
     }
 }
 
--- AdventOfCode/AdventOfCodeTests/Day1/Day1Tests.cs
+++ AdventOfCode/AdventOfCodeTests/Day1/Day1Tests.cs
@@ -8,17 +8,31 @@ namespace AdventOfCodeTests.Day1;
 public class Day1Tests
 {
     [Fact]
-    public void WorksForSampleData()
+    public void Part1_WorksForSampleData()
     {
         var sampleData = ParseInput(FileHelper.ReadFromFile("Day1", "Sample.txt"));
-        Assert.Equal(24000, Day1Puzzle.GetResult(sampleData));
+        Assert.Equal(24000, Day1Puzzle.GetHighestCalorieCountForASingleElf(sampleData));
     }
     
     [Fact]
-    public void WorksForRealData()
+    public void Part1_WorksForRealData()
     {
         var sampleData = ParseInput(FileHelper.ReadFromFile("Day1", "RealData.txt"));
-        Assert.Equal(71502, Day1Puzzle.GetResult(sampleData));
+        Assert.Equal(71502, Day1Puzzle.GetHighestCalorieCountForASingleElf(sampleData));
+    }
+    
+    [Fact]
+    public void Part2_WorksForSampleData()
+    {
+        var sampleData = ParseInput(FileHelper.ReadFromFile("Day1", "Sample.txt"));
+        Assert.Equal(45000, Day1Puzzle.GetTotalCaloriesCarriedByTop3Elves(sampleData));
+    }
+    
+    [Fact]
+    public void Part2_WorksForRealData()
+    {
+        var sampleData = ParseInput(FileHelper.ReadFromFile("Day1", "RealData.txt"));
+        Assert.Equal(208191, Day1Puzzle.GetTotalCaloriesCarriedByTop3Elves(sampleData));
     }
 
     public Elf[] ParseInput(string input)
