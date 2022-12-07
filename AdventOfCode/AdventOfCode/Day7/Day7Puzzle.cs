86c8a93fa6d44904f9aa91243263d38d51d3a0de
Tom Peters <tom.peters@octopus.com>
2022-12-07 19:33:40 +1000

    Day 7 - parsing input

--- AdventOfCode/AdventOfCode/Day7/Day7Puzzle.cs
+++ AdventOfCode/AdventOfCode/Day7/Day7Puzzle.cs
@@ -2,8 +2,88 @@ namespace AdventOfCode.Day7;
 
 public class Day7Puzzle
 {
-    public static int GetSumOfSizeOfDirectoriesGreaterThan(string input, int sizeThreshold)
+    public static int GetSumOfSizeOfDirectoriesGreaterThan(string instructionsString, int sizeThreshold)
     {
+        var instructions = ParseInstructions(instructionsString);
         return 0;
+    }
+
+    static IEnumerable<IInstruction> ParseInstructions(string instructions)
+    {
+        return instructions.Split("$", StringSplitOptions.RemoveEmptyEntries)
+            .Select(i => i.Trim())
+            .Select<string, IInstruction>(instructionString => {
+                if (instructionString.StartsWith("cd"))
+                {
+                    return new ChangeDirectoryInstruction(instructionString.Split("cd ")[1]);
+                }
+
+                return ParseListInstruction(instructionString);
+            });
+    }
+
+    static ListInstruction ParseListInstruction(string listInstructionInput)
+    {
+        var resultLines = listInstructionInput.Split("\n").Skip(1);
+        return new ListInstruction(resultLines.Select<string, IListResult>(l =>
+        {
+            if (l.StartsWith("dir"))
+            {
+                return new DirectoryListResult(l.Split("dir ")[1]);
+            }
+
+            var items = l.Split(" ");
+            return new FileListResult(items[1], int.Parse(items[0]));
+        }).ToArray());
+    }
+}
+
+public interface IInstruction
+{
+}
+
+public class ChangeDirectoryInstruction : IInstruction
+{
+    private readonly string _argument;
+
+    public ChangeDirectoryInstruction(string argument)
+    {
+        _argument = argument;
+    }
+}
+
+public class ListInstruction : IInstruction
+{
+    private readonly IListResult[] _listResults;
+
+    public ListInstruction(IListResult[] listResults)
+    {
+        _listResults = listResults;
+    }
+}
+
+public interface IListResult
+{
+}
+
+public class DirectoryListResult : IListResult
+{
+    private readonly string _directoryName;
+
+    public DirectoryListResult(string directoryName)
+    {
+        _directoryName = directoryName;
+    }
+}
+
+public class FileListResult : IListResult
+{
+    private readonly string _fileName;
+    private readonly int _fileSize;
+
+    public FileListResult(string fileName, int fileSize)
+    {
+        _fileName = fileName;
+        _fileSize = fileSize;
     }
 }
--- AdventOfCode/AdventOfCodeTests/Day7/Day7Tests.cs
+++ AdventOfCode/AdventOfCodeTests/Day7/Day7Tests.cs
@@ -8,12 +8,7 @@ public class Day7Tests
     [Fact]
     public void Part1_WorksForSampleData()
     {
-        var input = ParseInput(FileHelper.ReadFromFile("Day7", "Sample.txt"));
+        var input = FileHelper.ReadFromFile("Day7", "Sample.txt");
         Assert.Equal(95437, Day7Puzzle.GetSumOfSizeOfDirectoriesGreaterThan(input, 100000));
-    }
-
-    static string ParseInput(string input)
-    {
-        return input;
     }
 }
