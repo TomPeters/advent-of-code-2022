b67f88811c0a2ac0dc1ee36384982cdfa5141e37
Tom Peters <tom.peters@octopus.com>
2022-12-09 16:59:16 +1000

    Attempted part 1 - not working yet

--- AdventOfCode/AdventOfCode/Day8/Day8Puzzle.cs
+++ AdventOfCode/AdventOfCode/Day8/Day8Puzzle.cs
@@ -2,8 +2,48 @@ namespace AdventOfCode.Day8;
 
 public class Day8Puzzle
 {
-    public static int GetNumberOfVisibileTrees(string input)
+    public static int GetNumberOfVisibleTrees(Forest forest)
     {
-        return 0;
+        return forest.GetAllVisibleTrees().Count();
     }
+}
+
+public record Forest(Tree[] Trees)
+{
+    public IEnumerable<Tree> GetAllVisibleTrees()
+    {
+        var allDirections = (Direction[])Enum.GetValues(typeof(Direction));
+        return Trees.Where(t => allDirections.Any(t.IsVisibleFromDirection));
+    }
+}
+
+public class Tree
+{
+    private readonly int _height;
+    private readonly Dictionary<Direction, Tree> _adjacentTrees = new ();
+    
+    public Tree(int height)
+    {
+        _height = height;
+    }
+
+    public void RegisterAdjacentTree(Tree tree, Direction direction)
+    {
+        _adjacentTrees[direction] = tree;
+    }
+
+    public bool IsVisibleFromDirection(Direction direction)
+    {
+        var hasAdjacentTree = _adjacentTrees.TryGetValue(direction, out var adjacentTree);
+        if (!hasAdjacentTree) return true;
+        return adjacentTree!._height < _height && adjacentTree.IsVisibleFromDirection(direction);
+    }
+}
+
+public enum Direction
+{
+    Top,
+    Bottom,
+    Left,
+    Right
 }
--- AdventOfCode/AdventOfCodeTests/Day8/Day8Tests.cs
+++ AdventOfCode/AdventOfCodeTests/Day8/Day8Tests.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using AdventOfCode.Day8;
 using Xunit;
 
@@ -8,7 +9,37 @@ public class Day8Tests
     [Fact]
     public void Part1_WorksForSampleData()
     {
-        var input = FileHelper.ReadFromFile("Day8", "Sample.txt");
-        Assert.Equal(0, Day8Puzzle.GetNumberOfVisibileTrees(input));
+        var input = ParseInput(FileHelper.ReadFromFile("Day8", "Sample.txt"));
+        Assert.Equal(21, Day8Puzzle.GetNumberOfVisibleTrees(input));
+    }
+
+    static Forest ParseInput(string input)
+    {
+         var treeRows = input.Split("\n").Select(row =>
+         {
+             return row.Select(treeHeightChar =>
+             {
+                 var height = int.Parse(treeHeightChar.ToString());
+                 return new Tree(height);
+             }).ToArray();
+         }).ToArray();
+
+         var zipped = treeRows.Zip(treeRows.Skip(1), (treeRow, adjacentTreeRowBelow) => treeRow.Zip(adjacentTreeRowBelow));
+         foreach (var (treeAbove, treeBelow) in zipped.SelectMany(g => g))
+         {
+             treeAbove.RegisterAdjacentTree(treeBelow, Direction.Bottom);
+             treeBelow.RegisterAdjacentTree(treeAbove, Direction.Top);
+         }
+         
+         foreach (var treeRow in treeRows)
+         {
+             foreach (var (leftTree, rightTree) in treeRow.Zip(treeRow.Skip(1)))
+             {
+                 leftTree.RegisterAdjacentTree(rightTree, Direction.Right);
+                 rightTree.RegisterAdjacentTree(leftTree, Direction.Left);
+             }
+         }
+
+         return new Forest(treeRows.SelectMany(treeRow => treeRow).ToArray());
     }
 }
