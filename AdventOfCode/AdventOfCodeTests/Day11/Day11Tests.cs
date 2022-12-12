a4656c121163d406b046f11ca050b2594620d478
Tom Peters <tom.peters@octopus.com>
2022-12-12 20:12:43 +1000

    Day 11 part 2

--- AdventOfCode/AdventOfCodeTests/Day11/Day11Tests.cs
+++ AdventOfCode/AdventOfCodeTests/Day11/Day11Tests.cs
@@ -12,6 +12,12 @@ public class Day11Tests
     {
         Assert.Equal(10605, Day11Puzzle.GetLevelOfMonkeyBusiness(GetSampleMonkeys(), 20));
     }
+    
+    [Fact]
+    public void Part1_WorksForRealData()
+    {
+        Assert.Equal(95472, Day11Puzzle.GetLevelOfMonkeyBusiness(GetRealMonkeys(), 20));
+    }
 
     Monkeys GetSampleMonkeys()
     {
@@ -24,6 +30,22 @@ public class Day11Tests
             CreateMonkey(2, new []{79, 60, 97}, new SquareOperation(),
                 13, 1, 3),
             CreateMonkey(3, new []{74}, new AddOperation(3), 17, 0, 1),
+        };
+        return new Monkeys(monkeys);
+    }
+
+    Monkeys GetRealMonkeys()
+    {
+        var monkeys = new[]
+        {
+            CreateMonkey(0, new[] { 52, 60, 85, 69, 75, 75 }, new MultiplyOperation(17), 13, 6, 7),
+            CreateMonkey(1, new[] { 96, 82, 61, 99, 82, 84, 85 }, new AddOperation(8), 7, 0, 7),
+            CreateMonkey(2, new[] { 95, 79 }, new AddOperation(6), 19, 5, 3),
+            CreateMonkey(3, new[] { 88, 50, 82, 65, 77 }, new MultiplyOperation(19), 2, 4, 1),
+            CreateMonkey(4, new[] { 66, 90, 59, 90, 87, 63, 53, 88 }, new AddOperation(7), 5, 1, 0),
+            CreateMonkey(5, new[] { 92, 75, 62 }, new SquareOperation(), 3, 3, 4),
+            CreateMonkey(6, new[] { 94, 86, 76, 67 }, new AddOperation(1), 11, 5, 2),
+            CreateMonkey(7, new[] { 57 }, new AddOperation(2), 17, 6, 2)
         };
         return new Monkeys(monkeys);
     }
