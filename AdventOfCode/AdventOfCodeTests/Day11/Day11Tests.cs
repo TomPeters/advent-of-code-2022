97d1d8491a78a01388cc999852e85f97230bf880
Tom Peters <tom.peters@octopus.com>
2022-12-12 19:28:46 +1000

    Streamlined setup

--- AdventOfCode/AdventOfCodeTests/Day11/Day11Tests.cs
+++ AdventOfCode/AdventOfCodeTests/Day11/Day11Tests.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using AdventOfCode.Day11;
 using Xunit;
 
@@ -15,14 +17,20 @@ public class Day11Tests
     {
         var monkeys = new[]
         {
-            new Monkey(0, new[] { new Item(79), new Item(98) }, new MultiplyOperation(19),
-                new NextMonkeyTestParams(23, 2, 3)),
-            new Monkey(1, new[] { new Item(54), new Item(65), new Item(75), new Item(74) }, new AddOperation(6),
-                new NextMonkeyTestParams(19, 2, 0)),
-            new Monkey(2, new[] { new Item(79), new Item(60), new Item(97) }, new SquareOperation(),
-                new NextMonkeyTestParams(13, 1, 3)),
-            new Monkey(3, new[] { new Item(74) }, new AddOperation(3), new NextMonkeyTestParams(17, 0, 1)),
+            CreateMonkey(0, new []{79, 98}, new MultiplyOperation(19),
+                23, 2, 3),
+            CreateMonkey(1, new []{54, 65, 75, 74}, new AddOperation(6),
+                19, 2, 0),
+            CreateMonkey(2, new []{79, 60, 97}, new SquareOperation(),
+                13, 1, 3),
+            CreateMonkey(3, new []{74}, new AddOperation(3), 17, 0, 1),
         };
         return new Monkeys(monkeys);
+    }
+
+    static Monkey CreateMonkey(int monkeyId, IEnumerable<int> startingWorryLevels, IOperation operation,
+        int testDivisor, int trueMonkeyId, int falseMonkeyId)
+    {
+        new Monkey(monkeyId, startingWorryLevels.Select(l => new Item(l)).ToArray(), operation, new NextMonkeyTestParams(testDivisor, trueMonkeyId, falseMonkeyId)),
     }
 }
