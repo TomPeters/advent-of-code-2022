3e45d118d5e9745f63c5d5c85b8d373f7982e72f
Tom Peters <tom.peters@octopus.com>
2022-12-14 13:56:06 +1000

    WIP - Day 13 part 1

--- AdventOfCode/AdventOfCodeTests/Day13/Day13Tests.cs
+++ AdventOfCode/AdventOfCodeTests/Day13/Day13Tests.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using AdventOfCode.Day13;
 using Xunit;
 
@@ -15,6 +17,48 @@ public class Day13Tests
 
     private PacketPair[] ParseInput(string input)
     {
-        return Array.Empty<PacketPair>();
+        return input.Split("\n\n").Select(pairInput =>
+        {
+            var packets = pairInput.Split("\n").Select(ParseCompletePacket).ToArray();
+            return new PacketPair(packets[0], packets[1]);
+        }).ToArray();
     }
+
+    IPacket ParseCompletePacket(string input)
+    {
+        if (int.TryParse(input, out var parsedInt))
+        {
+            return new IntegerPacket(parsedInt);
+        }
+
+        var contentsExcludingEndBrackets = input.Substring(1, input.Length - 1);
+        var packetStrings = SplitByTopLevelComma(contentsExcludingEndBrackets);
+        var subPackets = packetStrings.Select(ParseCompletePacket);
+        return new ListPacket(subPackets.ToArray());
+    }
+
+    IEnumerable<string> SplitByTopLevelComma(string input)
+    {
+        var bracketsCount = 0;
+        var currentPacketString = "";
+        foreach(var currentChar in input)
+        {
+            if (currentChar == '[') bracketsCount++;
+            if (currentChar == ']') bracketsCount--;
+            if (currentChar == ',')
+            {
+                yield return currentPacketString;
+                currentPacketString = "";
+            }
+            else
+            {
+                currentPacketString += currentChar;
+            }
+        }
+
+        if (currentPacketString != "")
+        {
+            yield return currentPacketString;
+        }
+    } 
 }
