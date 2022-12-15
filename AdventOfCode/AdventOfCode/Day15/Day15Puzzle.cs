260ccac76d80f91ee5a7e44f53c7ead6c5c55d96
Tom Peters <tom.peters@octopus.com>
2022-12-15 20:53:31 +1000

    Day 15 parsing

--- AdventOfCode/AdventOfCode/Day15/Day15Puzzle.cs
+++ AdventOfCode/AdventOfCode/Day15/Day15Puzzle.cs
@@ -2,8 +2,18 @@ namespace AdventOfCode.Day15;
 
 public class Day15Puzzle
 {
-    public static int GetResult(string input)
+    public static int GetNumberOfPositionsThatCannotContainABeacon(AllMeasurements measurements, int rowNumber)
     {
         return 0;
     }
 }
+
+public record AllMeasurements(Measurement[] Measurements);
+
+public record Measurement(Sensor sensor, Beacon beacon);
+
+public record Beacon(Coordinate Coordinate);
+
+public record Sensor(Coordinate SensorCoordinate);
+
+public record Coordinate(int X, int Y);
--- AdventOfCode/AdventOfCodeTests/Day15/Day15Tests.cs
+++ AdventOfCode/AdventOfCodeTests/Day15/Day15Tests.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using AdventOfCode.Day15;
 using Xunit;
 
@@ -9,11 +10,32 @@ public class Day15Tests
     public void Part1_WorksForSampleData()
     {
         var input = ParseInput(FileHelper.ReadFromFile("Day15", "Sample.txt"));
-        Assert.Equal(26, Day15Puzzle.GetResult(input));
+        Assert.Equal(26, Day15Puzzle.GetNumberOfPositionsThatCannotContainABeacon(input, 10));
     }
 
-    private string ParseInput(string input)
+    private AllMeasurements ParseInput(string input)
     {
-        return input;
+        var measurements = input.Split("\n").Select(measurementInput =>
+        {
+            var parts = measurementInput.Split(":");
+            var sensorPart = parts[0];
+            var beaconPart = parts[1];
+            var sensorCoordinate = GetCoordinates(sensorPart);
+            var beaconCoordinate = GetCoordinates(beaconPart);
+            return new Measurement(new Sensor(sensorCoordinate), new Beacon(beaconCoordinate));
+        }).ToArray();
+        return new AllMeasurements(measurements);
+    }
+
+    Coordinate GetCoordinates(string inputWithCoordinates)
+    {
+        var startOfCoordinatePart = inputWithCoordinates.IndexOf("x=");
+        var coordinateString = inputWithCoordinates.Substring(startOfCoordinatePart);
+        var coordinateParts = coordinateString.Split(",");
+        var xString = coordinateParts[0];
+        var yString = coordinateParts[1];
+        var x = int.Parse(xString.Split("=")[1]);
+        var y = int.Parse(yString.Split("=")[1]);
+        return new Coordinate(x, y);
     }
 }
