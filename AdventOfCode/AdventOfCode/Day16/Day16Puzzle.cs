5545c844ab1871a9701ac011e3ddfce92b0e3033
Tom Peters <tom.peters@octopus.com>
2022-12-16 17:48:20 +1000

    Day 16 part 1 - build up a network of rooms

--- AdventOfCode/AdventOfCode/Day16/Day16Puzzle.cs
+++ AdventOfCode/AdventOfCode/Day16/Day16Puzzle.cs
@@ -2,8 +2,74 @@ namespace AdventOfCode.Day16;
 
 public class Day16Puzzle
 {
-    public static int GetTheMostPressureThatCanBeReleased(string input)
+    public static int GetTheMostPressureThatCanBeReleased(ScannedOutput scannedOutput)
     {
+        var network = Network.CreateNetwork(scannedOutput);
         return 0;
     }
 }
+
+public class Network
+{
+    private readonly Room _startingRoom;
+
+    public static Network CreateNetwork(ScannedOutput scannedOutput)
+    {
+        var allRooms = scannedOutput.ScannedRooms.Select(r => new Room(new Valve(r.Valve, r.FlowRate))).ToDictionary(r => r.Valve.Name);
+        var startingRoom = allRooms["AA"];
+        scannedOutput.ScannedRooms.ForEach(sr =>
+        {
+            sr.ConnectedValves.ForEach(roomName =>
+            {
+                Room.Connect(allRooms[sr.Valve], allRooms[roomName]);
+            });
+        });
+        return new Network(startingRoom);
+    }
+
+    Network(Room startingRoom)
+    {
+        _startingRoom = startingRoom;
+    }
+}
+
+public class Room
+{
+    protected bool Equals(Room other)
+    {
+        return Valve.Equals(other.Valve);
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(null, obj)) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        if (obj.GetType() != this.GetType()) return false;
+        return Equals((Room)obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return Valve.GetHashCode();
+    }
+
+    public Valve Valve { get; init; }
+    private readonly ISet<Room> _connectedRooms = new HashSet<Room>();
+
+    public Room(Valve valve)
+    {
+        Valve = valve;
+    }
+
+    public static void Connect(Room firstRoom, Room secondRoom)
+    {
+        firstRoom._connectedRooms.Add(secondRoom);
+        secondRoom._connectedRooms.Add(firstRoom);
+    }
+}
+
+public record Valve(string Name, int FlowRate);
+
+public record ScannedOutput(ScannedRoom[] ScannedRooms);
+
+public record ScannedRoom(string Valve, int FlowRate, string[] ConnectedValves);
--- AdventOfCode/AdventOfCodeTests/Day16/Day16Tests.cs
+++ AdventOfCode/AdventOfCodeTests/Day16/Day16Tests.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using AdventOfCode.Day16;
 using Xunit;
 
@@ -12,8 +14,23 @@ public class Day16Tests
         Assert.Equal(1651, Day16Puzzle.GetTheMostPressureThatCanBeReleased(input));
     }
 
-    private string ParseInput(string input)
+    private ScannedOutput ParseInput(string input)
     {
-        return input;
+        var scannedRooms = input.Split("\n").Select(roomInput =>
+        {
+            var parts = roomInput.Split(";");
+            var connectedRoomsInput = parts[1];
+            var valveInputParts = parts[0].Split(" has flow rate=");
+            var valveNameInput = valveInputParts[0];
+            var flowRate = int.Parse(valveInputParts[1]);
+            var valveName = valveNameInput.Split(" ")[1];
+            return new ScannedRoom(valveName, flowRate, GetConnectedRooms(connectedRoomsInput));
+        }).ToArray();
+        return new ScannedOutput(scannedRooms);
+    }
+
+    static string[] GetConnectedRooms(string connectedRoomsInput)
+    {
+        return connectedRoomsInput.Split(" ").Skip(5).Select(s => s.Replace(",", "")).ToArray();
     }
 }
